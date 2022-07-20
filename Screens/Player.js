f38433da81b9d6002499bcf0610ec9f9ce2cc241
Luigi Zottoli <lzottoli93@gmaail.com>
2022-07-20 19:48:05 +0200

    Apply Stack Navigation

---
+++ Screens/Player.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Player() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Player</Text>
+      </View>
+    );
+  }
