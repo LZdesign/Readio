1ac3afb16ec3030beaeb6b657a81c78417283d03
Luigi Zottoli <lzottoli93@gmaail.com>
2022-07-20 20:28:51 +0200

    Epub.js added to Player Screen

--- Screens/Player.js
+++ Screens/Player.js
@@ -1,27 +1,39 @@
 import * as React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, useWindowDimensions} from 'react-native';
+import {Reader, ReaderProvider} from 'epubjs-react-native';
 
 
-export default function Player({navigation, route}) {
-    return (
-      <View style={styles.container}>
-        <Text>Player</Text>
+export default function Player({navigation}) {
+    const {width, height} = useWindowDimensions();
+    return (        
+        <SafeAreaView style={styles.container}>
+            <Text>Player</Text>
+    
+            <Button title='Go to Library'
+                onPress={
+                    () => navigation.navigate('Library')
+                }/>
 
-        <Button
-        title='Go to Library'
-        onPress={() => navigation.navigate('Library')}
-        />
-      </View>
+                
+            <ReaderProvider>
+                <Reader src={
+                        {uri: 'https://s3.amazonaws.com/moby-dick/OPS/package.opf'}
+                    }
+                    width={width}
+                    height={height}/>
+            </ReaderProvider>
+        </SafeAreaView>
+
     );
-  }
+}
 
-  
-  const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
