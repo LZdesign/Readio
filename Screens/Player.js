89c674d8901e1f59ad3bc42f7ed38ee139854e6e
Luigi Zottoli <lzottoli93@gmaail.com>
2022-07-21 13:20:05 +0200

    Adding Epub File

--- Screens/Player.js
+++ Screens/Player.js
@@ -2,28 +2,35 @@ import * as React from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView, useWindowDimensions} from 'react-native';
 import {Reader, ReaderProvider} from 'epubjs-react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 
 export default function Player({navigation}) {
     const {width, height} = useWindowDimensions();
-    return (        
-        <SafeAreaView style={styles.container}>
-            <Text>Player</Text>
-    
-            <Button title='Go to Library'
-                onPress={
-                    () => navigation.navigate('Library')
-                }/>
+    return (
+        <ScrollView>
+            <View style={styles.container}>
 
-                
-            <ReaderProvider>
-                <Reader src={
-                        {uri: 'https://s3.amazonaws.com/moby-dick/OPS/package.opf'}
-                    }
-                    width={width}
-                    height={height}/>
-            </ReaderProvider>
-        </SafeAreaView>
+                <Button title='Go to Library'
+                    onPress={
+                        () => navigation.navigate('Library')
+                    }/>
+
+            </View>
+
+            <SafeAreaView style={
+                styles.container
+            }>
+
+                <ReaderProvider>
+                    <Reader src={{uri: 'https://s3.amazonaws.com/moby-dick/OPS/package.opf'}}
+                        width={width}
+                        height={height}
+                        initialLocation={'epubcfi(/6/2[cover]!/6)'}
+                        />
+                </ReaderProvider>
+            </SafeAreaView>
+        </ScrollView>
 
     );
 }
@@ -34,6 +41,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 20
     }
 });
